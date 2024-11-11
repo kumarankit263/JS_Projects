e74d31677a920c0e9de27e9e6c9c9e2ab269d1cf
Ankit Kumar <ankitiit263@gmail.com>
2024-11-11 23:10:18 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  fontFamily:{
+    Whitney:["Whitney"],
+    Ginto:["Ginto"],
+    ggSans:["ggSans"],
+  },
+}
